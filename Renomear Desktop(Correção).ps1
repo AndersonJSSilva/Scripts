bf2e98d61dbabcc2bf1a639670a11d85064dd750
AndersonJSSilva <105315956+AndersonJSSilva@users.noreply.github.com>
2024-08-12 21:29:38 -0300

    Add files via upload

---
+++ Renomear Desktop(Correção).ps1
@@ -0,0 +1,19 @@
+﻿#Variáveis Globais
+$Hostname = Get-content -Path C:\temp\LogIntune.txt
+$Usuario = "Domínio\renomeardesktop"
+
+#Download Arquivo de Senha (Primeira Opção)
+mkdir C:\temp
+(new-object System.Net.WebClient).DownloadFile(‘https://xxxx.blob.core.windows.net/renomeardesktop/.txt’,’C:\temp\.txt’)
+$Senha = "c:\temp\.txt"
+$SenhaCriptografada = Get-Content $Senha | ConvertTo-SecureString
+$Credencial = new-object -typename System.Management.Automation.PSCredential -argumentlist $Usuario, $SenhaCriptografada
+
+#KeyVault com Senha (Segunda Opção)
+#Cloud na Quebrada: https://www.youtube.com/watch?v=6Xt0AaD9420&t=1237s
+
+#Alterar Hostname
+Rename-Computer -NewName $Hostname -DomainCredential $Credencial
+
+#Remover Arquivos
+Remove-Item -Path C:\temp\.txt
---
+++ Renomear Desktop(Verificação).ps1
@@ -0,0 +1,24 @@
+﻿#Download Arquivo de Seriais
+mkdir C:\temp
+(new-object System.Net.WebClient).DownloadFile(‘https://xxxx.blob.core.windows.net/renomeardesktop/Seriais.txt’,’C:\temp\Seriais.txt’)
+
+#Criação do Blob
+#Microsoft 365 na Veia: https://www.youtube.com/watch?v=rT-Amdyao_Q
+
+#Variáveis Globais
+$Seriais = Get-content -Path c:\temp\seriais.txt
+$Hostname = Get-WmiObject Win32_ComputerSystem | Select-Object Name
+$SerialHostname = Get-WmiObject Win32_BIOS | Select SerialNumber
+
+##Avaliação de Hostname
+foreach($Serial in $Seriais){
+$SerialID = @($Serial -split ";")[0]
+$HostnameID = @($Serial -split ";")[1]
+    If ($SerialHostname.SerialNumber -eq $SerialID){
+        if ($Hostname.Name -eq $HostnameID){
+        exit 0}
+    else{
+    $Saida = $HostnameID
+    $Saida | Out-File -FilePath c:\temp\LogIntune.txt
+    exit 1}
+}}
